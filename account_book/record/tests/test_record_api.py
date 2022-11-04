6ba30d71c6fca4305c4b15a4a2faa4e48fe7f51d
omognuni <melbong0069@gmail.com>
2022-11-04 17:05:43 +0900

    [Test] record api 테스트

--- account_book/record/tests/test_record_api.py
+++ account_book/record/tests/test_record_api.py
@@ -5,14 +5,17 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from record.serializers import RecordSerializer
+from record.serializers import RecordSerializer, RecordDetailSerializer
 from core.models import Record
 
 
-BOOK_URL = reverse('book:book-list')
+RECORD_URL = reverse('record:record-list')
 
-def detail_url(book_id):
-    return reverse('book:book-detail', args=[book_id])
+def detail_url(record_id):
+    return reverse('record:record-detail', args=[record_id])\
+
+def delete_url(record_id):
+    return reverse('record:record-delete', args=[record_id])
 
 def create_record(user, **params):
     defaults = {
@@ -25,7 +28,7 @@ def create_record(user, **params):
     return Record.objects.create(**defaults)
 
 def create_user(email='test@gmail.com', password='testpass'):
-    return get_user_model(email=email, password=password)
+    return get_user_model().objects.create_user(email=email, password=password)
 
 class PublicAPITest(TestCase):
     '''비인증 사용자 테스트'''
@@ -36,7 +39,7 @@ class PublicAPITest(TestCase):
         '''인증 요구 테스트'''
         user = create_user()
         
-        res = self.client.get(BOOK_URL)
+        res = self.client.get(RECORD_URL)
         
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -48,7 +51,133 @@ class PrivateAPITest(TestCase):
         
         self.client.force_authenticate(self.user)
         
-    def test_create_record(self):
-        pass
+    def test_retrieve_record(self):
+        '''가계부 내역 가져오기'''
+        create_record(user=self.user)
+        create_record(user=self.user)
+        create_record(user=self.user)
         
+        res = self.client.get(RECORD_URL)
+        
+        records = Record.objects.all()
+        serializer = RecordSerializer(records, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
     
+    def test_record_list_limited_to_user(self):
+        '''유저가 작성한 가계부 내역만 가져오기'''
+        other_user = create_user(email='test2@gmail.com', password='testpass')
+        create_record(user=other_user)
+        create_record(user=self.user)
+
+        res = self.client.get(RECORD_URL)
+
+        records = Record.objects.filter(user=self.user)
+        serializer = RecordSerializer(records, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_record(self):
+        '''가계부 내역 생성'''
+        payload = {
+            'amount': 10000,
+            'memo': 'test memo',
+        }
+        
+        res = self.client.post(RECORD_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        record = Record.objects.get(id=res.data['id'])        
+        
+        for k, v in payload.items():
+            self.assertEqual(getattr(record, k), v)
+            
+    def test_view_record_detail(self):
+        """가계부 상세 내역"""
+        record = create_record(user=self.user)
+
+        url = detail_url(record.id)
+        res = self.client.get(url)
+
+        serializer = RecordDetailSerializer(record)
+        self.assertEqual(res.data, serializer.data)
+
+            
+    def test_partial_update(self):
+        '''가계부 내역 부분 업데이트'''
+        original_memo = 'test memo'
+        record = create_record(
+            user=self.user,
+            amount=1000,
+            memo=original_memo
+        )
+
+        payload = {
+            'amount': 100000
+        }
+
+        url = detail_url(record.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        record.refresh_from_db()
+        self.assertEqual(record.amount, payload['amount'])
+        self.assertEqual(record.memo, original_memo)
+        self.assertEqual(record.user, self.user)
+
+    def test_full_update(self):
+        '''가계부 내역 전체 업데이트'''
+        record = create_record(
+            user=self.user,
+            amount=10000,
+            memo='test memo'
+        )
+
+        payload = {
+            'amount': 1000,
+            'memo': 'test2 memo'
+        }
+        url = detail_url(record.id)
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        record.refresh_from_db()
+        for k, v in payload.items():
+            self.assertEqual(getattr(record, k), v)
+        self.assertEqual(record.user, self.user)
+        
+    def test_temp_delete_record(self):
+        '''가계부 임시 삭제 테스트'''
+        record = create_record(user=self.user)
+        
+        url = delete_url(record.id)
+        res = self.client.patch(url)
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        record.refresh_from_db()
+        self.assertTrue(record.is_deleted)
+        
+    def test_restore_record(self):
+        '''임시 삭제 내역 복구 테스트'''
+        record = create_record(user=self.user)
+        record.is_deleted = True
+        record.save()
+                
+        url = delete_url(record.id)
+        res = self.client.patch(url)
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        record.refresh_from_db()
+        self.assertFalse(record.is_deleted)
+        
+    def test_retrieve_without_deleted_record(self):
+        '''삭제한 내역 가져오지 않기'''
+        record = create_record(user=self.user, memo='Deleted', is_deleted=True)
+        create_record(user=self.user)
+        
+        res = self.client.get(RECORD_URL)
+        serializer = RecordSerializer(record)
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertNotIn(serializer.data, res.data)
+        
