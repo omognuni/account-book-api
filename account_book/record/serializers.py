f9b2e2f25cf15b2061547ca00b7b341604b4c91e
omognuni <melbong0069@gmail.com>
2022-11-04 18:20:29 +0900

    [Feat] Record 모델  필드 추가에 따른 변경

--- account_book/record/serializers.py
+++ account_book/record/serializers.py
@@ -7,7 +7,7 @@ class RecordSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Record
-        fields = ['id', 'amount', 'is_deleted']
+        fields = ['id', 'category', 'amount', 'is_deleted']
         read_only_fields = ['id']
         extra_kwargs = {'is_deleted': {'write_only': True}}
         
@@ -15,5 +15,5 @@ class RecordSerializer(serializers.ModelSerializer):
 class RecordDetailSerializer(RecordSerializer):
     '''내역 상세 보기에서 memo 확인'''
     class Meta(RecordSerializer.Meta):
-        fields = RecordSerializer.Meta.fields + ['memo']
-        
+        fields = RecordSerializer.Meta.fields + ['memo', 'created_at', 'updated_at']
+        read_only_fields = RecordSerializer.Meta.read_only_fields + ['created_at', 'updated_at']        
